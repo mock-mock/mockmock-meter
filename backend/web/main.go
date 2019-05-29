5a1505d8f1a5cb2eec62b178d3bbcee20e519baa
oshixtaku <>
2019-05-30 04:39:04 +0900

    Hello Vuetify Material Dashboard by Golang with echo

---
+++ backend/web/main.go
@@ -0,0 +1,26 @@
+/*
+
+ */
+
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	// Echoのインスタンス作る
+	e := echo.New()
+
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	//e.Static("/", "../../frontend/mockmockproto/dist")
+	e.Static("/", "../../frontend/vuetify-material-dashboard-master/dist")
+
+	// サーバー起動
+	e.Start(":8080")
+}
